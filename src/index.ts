f45a2b74567e73b4ca6d6c423e01267dd281c34b
Markis Taylor <m@rkis.cc>
2017-04-02 09:21:03 -0700

    upgrade dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import StripWhitespace from 'strip-whitespace';
 
 const stripWhitespace = new StripWhitespace();
 
-export default function (content: string) {
+export default function(content: string) {
   if (content) {
     const result = stripWhitespace.strip(content);
     if (result && result.replacements && result.replacements.length > 0) {
