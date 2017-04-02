e169960542ad3c3c413b0e4aad7e1f82ba5e2848
Markis Taylor <m@rkis.cc>
2017-04-02 09:03:50 -0700

    raise code coverage to 100%

--- test/index.ts
+++ test/index.ts
@@ -9,3 +9,21 @@ import StripWhitespaceLoader from '../src/index';
 
   ok(result === expected, `result does not equal expected.  Result: ${result}`);
 }
+
+{
+  const code = `const text = " x ";  `;
+  const expected = `const text = " x ";  `;
+
+  const result = StripWhitespaceLoader(code);
+
+  ok(result === expected, `result does not equal expected.  Result: ${result}`);
+}
+
+{
+  const code: string = null as any;
+  const expected = null;
+
+  const result = StripWhitespaceLoader(code);
+
+  ok(result === expected, `result does not equal expected.  Result: ${result}`);
+}
